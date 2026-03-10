526b157ec61671ea460c5579dc9fdd7fa72b2b48
Sukhman Sidhu <sidhu.sk@obmontessori.edu.ph>
2026-03-10 13:51:34 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// JavaScript Conditionals
+// function identify_sign () {
+    // num1 = document.getElementById('input1').value;
+
+   // if(num1 > 0) {
+    //    document.getElementById('output').innerHTML = 'It is positive'; 
+  //  } else if (num1 < 0) {
+  //   document.getElementById('output').innerHTML = 'It is negative';
+  //  } else {
+  //      document.getElementById('output').innerHTML = 'It is zero';
+   // }
+
+   function odd_or_even() {
+    num1 = document.getElementById('input1').value;
+
+    if (num1 % 2 == 0) {
+        window.alert('It is an even number')
+    }  else {
+        window.alert('It is an odd number')
+    }
+   }
